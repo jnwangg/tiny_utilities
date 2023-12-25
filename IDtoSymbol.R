312bb6bc8bdb280ef8ad22a25702811eb9bb7f5f
jnwangg <zwang474@usc.edu>
2023-12-24 21:34:34 -0800

    Implement gene ID extraction.

---
+++ IDtoSymbol.R
@@ -0,0 +1,9 @@
+library(Seurat)
+library(biomaRt)
+
+# Takes a Seurat object and converts Ensembl gene IDs to gene symbols in-place.
+IdtoSymbol <- function(seurObj) {
+  # Retrieve gene IDs from the given Seurat object and removes version numbers.
+  geneIDs <- rownames(seurObj@assays[["RNA"]]@meta.features)
+  geneIDs <- gsub("\\.\\d+$", "", geneIDs)
+}
