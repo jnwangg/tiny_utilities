6b86c97a4f93650063a81758a7d6b7359ee9e6c4
jnwangg <zwang474@usc.edu>
2023-12-28 13:52:50 -0800

    Add categorical column for source of each matrix

--- ConcatMatrices/ConcatMatrices.py
+++ ConcatMatrices/ConcatMatrices.py
@@ -5,21 +5,18 @@ import anndata as ad
 def ConcatMatrices(inDir, outDir):
    os.makedirs(outDir, exist_ok=True)
 
-   pathList = []
+   objList = {}
    for sub, _, files in os.walk(inDir):
        if "counts_unfiltered" in sub and "adata.h5ad" in files:
            objPath = os.path.join(sub, "adata.h5ad")
-           pathList.append(objPath)
+           sample = os.path.basename(os.path.dirname(sub))
+           objList[sample] = ad.read_h5ad(objPath)
 
-   objList = []
-   for objPath in pathList:
-       obj = ad.read_h5ad(objPath)
-       objList.append(obj)
-
-   objConcat = ad.concat(objList, join="outer", label="cell", index_unique="-")
+   objConcat = ad.concat(objList, join="outer", label="sample", index_unique="-")
 
    output_filename = os.path.join(outDir, "concatenated.h5ad")
    objConcat.write(output_filename)
+
 
 if __name__ == "__main__":
    parser = argparse.ArgumentParser(description="Aggregate multiple count matrices from kallisto-bustools output.")
