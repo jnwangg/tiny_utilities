388255e4559c100511c0d79d12a2cbe38b6c50e3
jnwangg <zwang474@usc.edu>
2023-12-28 14:30:03 -0800

    Add in-line documentation

--- ConcatMatrices/ConcatMatrices.py
+++ ConcatMatrices/ConcatMatrices.py
@@ -3,35 +3,55 @@ import argparse
 import anndata as ad
 
 def ConcatMatrices(inDir, outDir, colLabel, outFile):
-   os.makedirs(outDir, exist_ok=True)
+    """
+    Concatenates multiple count matrices from kallisto-bustools output.
+    Produces a concatenated Anndata object.
 
-   objList = {}
-   for sub, _, files in os.walk(inDir):
-       if "counts_unfiltered" in sub and "adata.h5ad" in files:
-           objPath = os.path.join(sub, "adata.h5ad")
-           sample = os.path.basename(os.path.dirname(sub))
-           objList[sample] = ad.read_h5ad(objPath)
+    Args:
+        inDir (str): Input directory with kallisto-bustools output.
+        outDir (str): Output directory to store concatenated anndata object.
+        colLabel (str): Column label for the original source of each matrix.
+        outFile (str): Name to use for concatenated anndata object.
+    """
+    # Create output dir, if it does not already exist.
+    os.makedirs(outDir, exist_ok=True)
 
-   objConcat = ad.concat(objList, join="outer", label=colLabel, index_unique="-")
+    # Store mapping of subdir name to matrice.
+    objList = {}
 
-   output_filename = os.path.join(outDir, outFile)
-   objConcat.write(output_filename)
+    # Walk through each subdir. For every count matrice found:
+    for sub, _, files in os.walk(inDir):
+        if "counts_unfiltered" in sub and "adata.h5ad" in files:
+            # Read in count matrice. Map matrice to the name of its subdir.
+            objPath = os.path.join(sub, "adata.h5ad")
+            sample = os.path.basename(os.path.dirname(sub))
+            objList[sample] = ad.read_h5ad(objPath)
+
+    # Concatenate matrices, making indices unique if needed.
+    objConcat = ad.concat(objList, join="outer", label=colLabel, index_unique="-")
+
+    # Save concatenated object.
+    output_filename = os.path.join(outDir, outFile)
+    objConcat.write(output_filename)
 
 
 if __name__ == "__main__":
-   parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-                                    description="Concatenates multiple count matrices from kallisto-bustools output. \
-                                    Produces a concatenated Anndata object.")
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                        description="Concatenates multiple count matrices from \
+                                        kallisto-bustools output. Produces a concatenated Anndata object.")
 
-   parser.add_argument("inDir", type=str,
-                       help="input directory with kallisto-bustools output.")
-   parser.add_argument("outDir", type=str,
-                       help="output directory to store concatenated anndata object")
-   parser.add_argument("-l", "--label", type=str, metavar="", default="sample",
-                       help="column label for the original source of each matrix")
-   parser.add_argument("-o", "--out", type=str, metavar="", default="concat.h5ad",
-                       help="name to use for concatenated anndata object")
+    # Positional arguments.
+    parser.add_argument("inDir", type=str,
+                        help="input directory with kallisto-bustools output.")
+    parser.add_argument("outDir", type=str,
+                        help="output directory to store concatenated anndata object")
 
-   args = parser.parse_args()
+    # Optional arguments.
+    parser.add_argument("-l", "--label", type=str, metavar="", default="sample",
+                        help="column label for the original source of each matrix")
+    parser.add_argument("-o", "--out", type=str, metavar="", default="concat.h5ad",
+                        help="name to use for concatenated anndata object")
 
-   ConcatMatrices(args.inDir, args.outDir, args.label, args.out)
+    # Parse arguments and concatenate.
+    args = parser.parse_args()
+    ConcatMatrices(args.inDir, args.outDir, args.label, args.out)
