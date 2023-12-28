2e79f59974946fc0c386c508ac6668cef2bdccfb
jnwangg <zwang474@usc.edu>
2023-12-28 14:17:48 -0800

    Allow specification of batch label and output file name

--- ConcatMatrices/ConcatMatrices.py
+++ ConcatMatrices/ConcatMatrices.py
@@ -2,7 +2,7 @@ import os
 import argparse
 import anndata as ad
 
-def ConcatMatrices(inDir, outDir):
+def ConcatMatrices(inDir, outDir, colLabel, outFile):
    os.makedirs(outDir, exist_ok=True)
 
    objList = {}
@@ -12,16 +12,26 @@ def ConcatMatrices(inDir, outDir):
            sample = os.path.basename(os.path.dirname(sub))
            objList[sample] = ad.read_h5ad(objPath)
 
-   objConcat = ad.concat(objList, join="outer", label="sample", index_unique="-")
+   objConcat = ad.concat(objList, join="outer", label=colLabel, index_unique="-")
 
-   output_filename = os.path.join(outDir, "concatenated.h5ad")
+   output_filename = os.path.join(outDir, outFile)
    objConcat.write(output_filename)
 
 
 if __name__ == "__main__":
-   parser = argparse.ArgumentParser(description="Aggregate multiple count matrices from kallisto-bustools output.")
-   parser.add_argument("inDir", type=str, help="Input directory with kallisto-bustools output.")
-   parser.add_argument("outDir", type=str, help="Output directory to store concatenated Anndata object.")
+   parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                    description="Concatenates multiple count matrices from kallisto-bustools output. \
+                                    Produces a concatenated Anndata object.")
+
+   parser.add_argument("inDir", type=str,
+                       help="input directory with kallisto-bustools output.")
+   parser.add_argument("outDir", type=str,
+                       help="output directory to store concatenated anndata object")
+   parser.add_argument("-l", "--label", type=str, metavar="", default="sample",
+                       help="column label for the original source of each matrix")
+   parser.add_argument("-o", "--out", type=str, metavar="", default="concat.h5ad",
+                       help="name to use for concatenated anndata object")
+
    args = parser.parse_args()
 
-   ConcatMatrices(args.inDir, args.outDir)
+   ConcatMatrices(args.inDir, args.outDir, args.label, args.out)
